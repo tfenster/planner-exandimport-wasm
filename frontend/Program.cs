1ec3f02bab73ca682a9e46b9f95b5ecb13a37393
Tobias Fenster <tfenster@4psbau.de>
2023-01-07 22:30:48 +0000

    add ant blazor

--- frontend/Program.cs
+++ frontend/Program.cs
@@ -1,12 +1,14 @@
 using Microsoft.AspNetCore.Components;
 using Microsoft.AspNetCore.Components.Web;
 using frontend.Data;
+using AntDesign;
 
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
 builder.Services.AddRazorPages();
 builder.Services.AddServerSideBlazor();
+builder.Services.AddAntDesign();
 builder.Services.AddSingleton<WeatherForecastService>();
 
 var app = builder.Build();
